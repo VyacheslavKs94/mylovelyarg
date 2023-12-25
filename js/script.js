7e46d17ebaa5b7818afb83be140c051fdd53c2b5
Slava <mail@slavaks.me>
2023-12-26 00:19:56 +0100

    js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,38 @@
-document.getElementById('reveal-button').addEventListener('click', function() {
-    var userLang = navigator.language || navigator.userLanguage; 
-    userLang = userLang.split('-')[0]; // Extract the language code
-
-    // Redirect based on language code
-    switch(userLang) {
-        case 'ru': // Russian
-            window.location.href = 'ctao_ru.html';
-            break;
-        case 'es': // Spanish
-            window.location.href = 'ctao_es.html';
-            break;
-        case 'tr': // Turkish
-            window.location.href = 'ctao_tr.html';
-            break;
-        case 'en': // English
-            window.location.href = 'ctao.html';
-            break;
-        case 'de': // German
-            window.location.href = 'ctao_de.html';
-            break;
-        case 'fr': // French
-            window.location.href = 'ctao_fr.html';
-            break;
-        default: // Default to English
-            window.location.href = 'ctao.html';
-    }
-});
-
 document.addEventListener('DOMContentLoaded', function() {
+    // Redirection based on language
+    var revealButton = document.getElementById('reveal-button');
+    if (revealButton) {
+        revealButton.addEventListener('click', function() {
+            var userLang = navigator.language || navigator.userLanguage; 
+            userLang = userLang.split('-')[0]; // Extract the language code
+
+            // Redirect based on language code
+            switch(userLang) {
+                case 'ru': // Russian
+                    window.location.href = 'ctao_ru.html';
+                    break;
+                case 'es': // Spanish
+                    window.location.href = 'ctao_es.html';
+                    break;
+                case 'tr': // Turkish
+                    window.location.href = 'ctao_tr.html';
+                    break;
+                case 'en': // English
+                    window.location.href = 'ctao.html';
+                    break;
+                case 'de': // German
+                    window.location.href = 'ctao_de.html';
+                    break;
+                case 'fr': // French
+                    window.location.href = 'ctao_fr.html';
+                    break;
+                default: // Default to English
+                    window.location.href = 'ctao.html';
+            }
+        });
+    }
+
+    // Typing effect for message
     var pageLanguage = document.documentElement.lang; // Get page language
     var messages = {
         'en': "Confidential Transmission to All Operatives\n\nAttention Operatives, ... [English text here]",
