aaaadd4dcefe316ce98260a5b5d6b9b6add6ad82
Slava <mail@slavaks.me>
2023-12-26 00:16:25 +0100

    New pages

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 document.getElementById('reveal-button').addEventListener('click', function() {
-    window.location.href = 'ctao.html';
-});
-
-document.getElementById('reveal-button').addEventListener('click', function() {
     var userLang = navigator.language || navigator.userLanguage; 
     userLang = userLang.split('-')[0]; // Extract the language code
 
@@ -18,7 +14,7 @@ document.getElementById('reveal-button').addEventListener('click', function() {
             window.location.href = 'ctao_tr.html';
             break;
         case 'en': // English
-            window.location.href = 'ctao_en.html';
+            window.location.href = 'ctao.html';
             break;
         case 'de': // German
             window.location.href = 'ctao_de.html';
@@ -27,6 +23,40 @@ document.getElementById('reveal-button').addEventListener('click', function() {
             window.location.href = 'ctao_fr.html';
             break;
         default: // Default to English
-            window.location.href = 'ctao_en.html';
+            window.location.href = 'ctao.html';
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var pageLanguage = document.documentElement.lang; // Get page language
+    var messages = {
+        'en': "Confidential Transmission to All Operatives\n\nAttention Operatives, ... [English text here]",
+        'es': "Transmisión confidencial a todos los operativos\n\nAtención operativos, ... [Spanish text here]",
+        'ru': "Абоба",
+        'tr': "... [Turkish text here]",
+        'de': "... [German text here]",
+        'fr': "... [French text here]"
+        // Add other languages as needed
+    };
+
+    var hasVisited = localStorage.getItem('hasVisited-' + pageLanguage);
+    var messageContainer = document.getElementById('message-container');
+    var message = messages[pageLanguage] || messages['en']; // Default to English if not found
+
+    if (!hasVisited) {
+        var i = 0;
+
+        function typeWriter() {
+            if (i < message.length) {
+                messageContainer.innerHTML += message.charAt(i);
+                i++;
+                setTimeout(typeWriter, 50); // Adjust typing speed here
+            }
+        }
+
+        typeWriter();
+        localStorage.setItem('hasVisited-' + pageLanguage, 'true');
+    } else {
+        messageContainer.innerText = message;
     }
 });
