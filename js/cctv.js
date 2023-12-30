fd19d1c94bfe978c1ef9b5c4a517e3d741be740f
Slava <mail@slavaks.me>
2023-12-30 22:20:39 +0100

    Fixes

--- js/cctv.js
+++ js/cctv.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const phrases = [
-        'In a digital world, I am three kings,',
-        'Masters of display, in color they sing.',
-        'The first rules the <span style="color: red;">dawn</span> with a fiery might,',
+        
         'The second commands the <span style="color: green;">forest</span> in light,',
-        'The third in the <span style="color: lightblue;">ocean</span> depth takes flight.',
-        'Together, they bring the spectrum to sight.'
+        'In a digital world, I am three kings,',
+        'The first rules the <span style="color: red;">dawn</span> with a fiery might,',
+        'Together, they bring the spectrum to sight.',
+        'Masters of display, in color they sing.',
+        'The third in the <span style="color: lightblue;">ocean</span> depth takes flight.'
+        
     ];
 
     const mosaicBackground = document.querySelector('.mosaic-background');
