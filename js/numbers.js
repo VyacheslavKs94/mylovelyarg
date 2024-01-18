c1f301a86afb4fd15c2119639c1bb8c412db272d
Slava <mail@slavaks.me>
2024-01-18 23:58:07 +0100

    Chisla

---
+++ js/numbers.js
@@ -0,0 +1,15 @@
+function updateProgress(barId, currentActivations, totalActivations) {
+    const percentage = (currentActivations / totalActivations) * 100;
+    const bar = document.getElementById(barId).querySelector('.progress');
+    const span = bar.parentNode.querySelector('span'); 
+    bar.style.width = percentage + '%';
+    span.textContent = span.textContent.split(':')[0] + ': ' + currentActivations + '/' + totalActivations;
+}
+
+updateProgress('firewallBreach', 333, 333); 
+updateProgress('coreDump', 333, 333); 
+updateProgress('systemOverride', 333, 333); 
+updateProgress('networkInfiltration', 7, 333); 
+updateProgress('encryptionBreak', 0, 333); 
+updateProgress('bioMads', 2000, 2000); 
+updateProgress('secret', 0, 10000); 
